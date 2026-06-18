d8a260038e828b4793d0153a772397ed02cb4917
otavio6 <otaviogoncalves.contato@gmail.com>
2026-06-18 17:00:44 -0300

    feat: add Google Tag Manager GTM-P36NZ8J6 to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Montserrat } from "next/font/google"
+import Script from "next/script"
 import "./globals.css"
 
 const montserrat = Montserrat({
@@ -11,10 +12,29 @@ const montserrat = Montserrat({
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html suppressHydrationWarning className={montserrat.variable}>
+      <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="gtm-head"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-P36NZ8J6');`,
+          }}
+        />
+      </head>
       <body
         className={`${montserrat.className} bg-creme text-charcoal antialiased`}
         suppressHydrationWarning
       >
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P36NZ8J6"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
       </body>
     </html>
